1262dbd6d62ee5d942ecd6611ad100e8fe91a675
Miguel Oller <maoa22@gmail.com>
2017-01-30 10:52:55 -0500

    Update resolveThunk.js

--- src/build/resolveThunk.js
+++ src/build/resolveThunk.js
@@ -1,3 +1,7 @@
-export default function resolveThunk(thunk) {
+/* @flow */
+
+export type Thunk<T> = (() => T) | T;
+
+export default function resolveThunk<T>(thunk: Thunk<T>): T {
   return typeof thunk === 'function' ? thunk() : thunk;
 }
